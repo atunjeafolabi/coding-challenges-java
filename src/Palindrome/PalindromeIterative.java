fc57d3eb3dbe78ba3e64f63651574a858b93d3c1
Atunje <atunje_afolabi@yahoo.com>
2021-11-01 11:01:06 +0100

    palindrome implementation

---
+++ src/Palindrome/PalindromeIterative.java
@@ -0,0 +1,21 @@
+package Palindrome;
+
+public class PalindromeIterative {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("madam"));
+    }
+
+    public static boolean isPalindrome(String text) {
+        String clean = text.replaceAll("\\s+", "").toLowerCase();
+        int length = clean.length();
+        int forward = 0;
+        int backward = length - 1;
+        while (backward > forward) {
+            char forwardChar = clean.charAt(forward++);
+            char backwardChar = clean.charAt(backward--);
+            if (forwardChar != backwardChar)
+                return false;
+        }
+        return true;
+    }
+}
